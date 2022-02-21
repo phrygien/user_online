8f10c7f15a5085c11f0552ea8ee2412b7317faf8
mecene <mecenephrygien@gmail.com>
2022-02-21 11:40:53 +0300

    mis a jour routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('status', [UserController::class, 'userOnlineStatus']);
